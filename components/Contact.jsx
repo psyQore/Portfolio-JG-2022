c70b6bf1589fbd513e7212526892a75ee689f81c
Juan Galarce <j6alarce@gmail.com>
2022-07-23 16:50:47 -0400

    Se crea un icono para subir al home

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 import contactImg from "../public/assets/contact.jpg";
 import { AiOutlineMail } from "react-icons/ai";
 import { FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import { HiOutlineChevronDoubleUp } from "react-icons/hi";
 
 const Contact = () => {
   return (
@@ -95,9 +97,19 @@ const Contact = () => {
                     rows="10"
                   ></textarea>
                 </div>
+                <button className="w-full p-4 text-gray-100 mt-4">
+                  Send Message
+                </button>
               </form>
             </div>
           </div>
+        </div>
+        <div className="flex justify-center py-12">
+          <Link href="/">
+            <div className="rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300">
+              <HiOutlineChevronDoubleUp className="text-[#5651e5]" size={30}/>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
